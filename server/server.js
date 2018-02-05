87f4145a5a2fb40b6ace8c0fb8ced3cd55b8f106
karthik Hegde <karthikhegde77@gmail.com>
2018-02-05 12:49:40 +0530

    added connections

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 const path = require('path');
 const express = require('express');
-const publicPath = path.join(__dirname,'../public');
+const http = require('http');
+const socketIO = require('socket.io');
 
-const app = express();
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+const app = express();
+let server = http.createServer(app);
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+let io = socketIO(server);
+io.on('connection',(socket)=>{
+  console.log('new user connected');
+
+  socket.on('disconnect',()=>{
+    console.log('user disconnected');
+  })
+});
+server.listen(port, () => {
   console.log(`Started on port ${port}`);
 })
